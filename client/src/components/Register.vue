0b9930543ef1510638a341ab5d6ee2355a4b5588
Milton Pauta <>
2019-07-18 15:28:45 -0400

    test backend-frontend register route

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,20 +1,37 @@
 <template>
     <div>
         <h1>Register</h1>
-    
-
-    
-        <input type="email" name="email" placeholder="enter your email here"/> 
-        <!-- eslint-disable-next-line -->
-        <input type="password" name="password" placeholder="enter your password here"/> 
+        <input type="email" name="email" v-model="email" placeholder="email"> 
+        <input type="password" name="password" v-model="password" placeholder="password"> 
+        <button @click="register" type="submit">Register Now</button>
     </div>
 </template>
 
 <script>
+import AuthenticationService from '../services/AuthenticationService' 
+import { setTimeout } from 'timers';
 export default {
   data () {
     return {
+        email: '',
+        password: '' 
     }
+  }, 
+  methods: {
+      async register(){
+        //   wait for response back from server 
+          const response = await AuthenticationService.register({
+              email: this.email, 
+              password: this.password
+          })
+          console.log(response.data); 
+      }
+  },
+  mounted(){
+     
+  },
+  watch:{
+    
   }
 }
 </script>
